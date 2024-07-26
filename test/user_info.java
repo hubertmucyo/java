0e499e10033517a4af1c556b20a71c860512c310
Hubert Mucyo <h.mucyo@alustudent.com>
2024-07-26 09:43:16 +0530

    simple_print

---
+++ test/user_info.java
@@ -0,0 +1,13 @@
+package test;
+
+public class user_info {
+    public static void main(String [] args){
+        System.out.println("USER INFORMATION\n"+"--------------------------------------");
+        System.out.println("NAME    : SUNIL SILVA");
+        System.out.println("ADDRESS : N023, panadura");
+        System.out.println("AGE     : 19");
+        System.out.println("SALARY  : 23500");
+        System.out.println("GENDER  : M\n"+"----------------------------------------------");
+
+    }
+}
---
+++ test/welcome2.java
@@ -0,0 +1,8 @@
+package test;
+
+public class welcome2 {
+    public static void main(String[] args){
+    System.out.println("Welcome\n"+"to\n"+"Java\n"+"Programming!!");
+    }
+}
+
