6346c142fcc282f715829260ba13696ee3b32eee
Hubert Mucyo <h.mucyo@alustudent.com>
2024-07-26 10:09:46 +0530

    simple_print

--- test/user_info.java
+++ test/user_info.java
@@ -1,13 +1,30 @@
 package test;
-
+import java.util.Scanner; 
 public class user_info {
+   
     public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        String name;
+        System.out.println("Enter your name: ");
+        name=sc.nextLine();
+        int age;
+        System.out.println("ENter your age: ");
+        age=sc.nextInt();
+        String address;
+        System.out.println("Enter your address: ");
+        address=sc.nextLine();
+        float salary;
+        System.out.println("Enter your salary: ");
+        salary=sc.nextFloat();
+        String gender;
+        gender=sc.nextLine();
+
         System.out.println("USER INFORMATION\n"+"--------------------------------------");
-        System.out.println("NAME    : SUNIL SILVA");
-        System.out.println("ADDRESS : N023, panadura");
-        System.out.println("AGE     : 19");
-        System.out.println("SALARY  : 23500");
-        System.out.println("GENDER  : M\n"+"----------------------------------------------");
+        System.out.println("NAME    : "+name);
+        System.out.println("ADDRESS : "+address);
+        System.out.println("AGE     :"+age);
+        System.out.println("SALARY  : "+salary);
+        System.out.println("GENDER  : "+gender+"-------------------------------------------");
 
     }
 }
