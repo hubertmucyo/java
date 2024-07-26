4c9fdb5ce59f4a62c1572e2b7bebaa0433b1113c
Hubert Mucyo <h.mucyo@alustudent.com>
2024-07-26 10:29:24 +0530

    sum

---
+++ test/sum.java
@@ -0,0 +1,17 @@
+package test;
+import java.util.Scanner;
+public class sum {
+    public static void main(String [] args){
+        Scanner subject=new Scanner(System.in);
+        float[] subj =new float[5];
+        float sum=0;
+        System.out.println("Enter marks of 5 subjects:\n");
+        for(int i=0;i<5;i++){
+            subj[i]=subject.nextFloat();
+            sum=sum+subj[i];
+        }
+        
+        System.out.println("The sum is: "+sum);
+
+    }
+}
